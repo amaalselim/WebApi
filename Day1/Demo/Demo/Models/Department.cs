5b4eda1dc164cb769a8c1e4b39b2c3dc701e6e78
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-09-11 19:41:03 +0300

    Done

---
+++ Day1/Demo/Demo/Models/Department.cs
@@ -0,0 +1,9 @@
+﻿namespace Demo.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Manager { get; set; }
+    }
+}
