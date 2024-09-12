abce47622e01a2afbfc546bb4fc8ea82c42ccc89
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-09-12 22:57:34 +0300

    Delete Demo directory

--- Demo/Demo/DTO/EmployeeWithDepartmentNameDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Demo.DTO
-{
-    public class EmployeeWithDepartmentNameDTO
-    {
-        public int Id { get; set; }
-        public string EmployeeName { get; set; }
-        public string DepartmentName { get; set; }
-    }
-}
